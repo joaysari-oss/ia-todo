caaa112f48c8fa9d55e5cab9cbeb6a6d5f1da50d
joaysari-oss <joaysari@gmail.com>
2026-08-09 14:48:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-import os
-from flask import Flask, render_template
-import google.generativeai as genai
+from flask import Flask, render_template, request
+import brain
 
-app = Flask(__name__, template_folder='interface')
-
-# Configura tu API Key (Render la tomará del entorno)
-genai.configure(api_key=os.environ.get("GOOGLE_API_KEY"))
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/ask', methods=['POST'])
+def ask():
+    user_input = request.form['query']
+    response = brain.generate_response(user_input)
+    return response
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
