2e80d4ca695a29e3a94707bc48501541722771a7
joaysari-oss <joaysari@gmail.com>
2026-08-09 15:19:43 -0500

    Update brain.py

--- brain.py
+++ brain.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 api_key = os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('models/gemini-1.5-flash')
 
 def generate_response(prompt):
     response = model.generate_content(prompt)
